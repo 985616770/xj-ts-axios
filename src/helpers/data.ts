5225f60e6a73b3637f1ed32091db6d2b1d6cfd9e
xuanji <z985616770@gmail.com>
2020-09-09 22:30:32 +0800

    重新开始项目

--- src/helpers/data.ts
+++
@@ -1,9 +0,0 @@
-import { isPlainObject } from './util'
-
-export function transformRequest(data: any): any {
-  if (isPlainObject(data)) {
-    // 是普通对象就进行JSON化
-    return JSON.stringify(data)
-  }
-  return data
-}
